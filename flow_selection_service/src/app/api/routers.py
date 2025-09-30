7535c205915f07331408d2d71eb12dc6dacf768e
Kozlov Leonid <jimjpoggers@yandex.ru>
2025-09-30 22:20:09 +0700

    Add prefix /api to endpoints

--- flow_selection_service/src/app/api/routers.py
+++ flow_selection_service/src/app/api/routers.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from app.api.endpoints import product_router
 
 
-main_router = APIRouter()
+main_router = APIRouter(prefix='/api')
 main_router.include_router(product_router)
