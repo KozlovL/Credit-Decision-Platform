c3b9ae06798775b0908cf673e8f862afa2cb1e9a
Leonid Kozlov <jimjpoggers@yandex.ru>
2025-11-03 14:14:39 +0700

    Change test postgres env

--- user-data-service/src/app/constants.py
+++ user-data-service/src/app/constants.py
@@ -18,8 +18,8 @@ AVAILABLE_PRODUCTS = (
 )
 TEST_DATABASE_URL = (
     f'postgresql+asyncpg://'
-    f'{os.getenv("TEST_USER")}:{os.getenv("TEST_PASSWORD")}'
-    f'@127.0.0.1:5433/{os.getenv("TEST_DB")}'
+    f'{os.getenv("POSTGRES_TEST_USER")}:{os.getenv("POSTGRES_TEST_PASSWORD")}'
+    f'@127.0.0.1:5433/{os.getenv("POSTGRES_TEST_DB")}'
 )
 CONFIG_PATH = os.path.join(
     os.path.dirname(__file__),
